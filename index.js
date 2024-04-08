d1a2f1b32c13153754c4ee5a1372e82b945396c8
C-a-n-j <eddymacharia450@gmail.com>
2024-04-08 10:12:27 +0300

    complete-lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // code your solution here
+
+//function satudayfun
+
+function saturdayFun(activity = `roller-skate`) {
+    return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun());
+console.log(saturdayFun(`bathe my dog`));
+
+//fuction mondaywork
+
+function mondayWork(action = `go to the office` ){
+    return `This Monday, I will ${action}.`
+
+}
+console.log(mondayWork());
+console.log(mondayWork(`work from home`));
+
+
+//fuction wrapadjective
+
+// function wrapAdjective(adjective = "*"){
+//     let result = wrapAdjective(adjective)
+//     let emphatic ="a hard worker"
+//     return `You are ${adjective}${emphatic}${adjective}!`
+// }
+
+function wrapAdjective(special = "*"){
+    function emphatic(comp = "a hard worker"){
+        let result = `${special}${comp}${special}`
+        return `You are ${result}!`
+
+    }
+    return emphatic
+}
+console.log(wrapAdjective('*'))
